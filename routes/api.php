ed22b32f3bb34ece4a0bfe319137e8825c3ea1fa
jos3duardo <jos3duardolopes@gmail.com>
2020-05-13 22:15:18 -0400

    alter method request

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::post('/login', 'UserController@login');
 Route::post('/register', 'UserController@register');
 Route::middleware('auth:sanctum')->put('/profile', 'UserController@profile');
 Route::middleware('auth:sanctum')->post('/content/add', 'ContentController@store');
-Route::middleware('auth:sanctum')->post('/content/list', 'ContentController@index');
+Route::middleware('auth:sanctum')->get('/content/list', 'ContentController@index');
